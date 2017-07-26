b5e196e16db7fa366902f1a64a73b65f1fa963d3
astroash <astroaisha@gmail.com>
2017-07-26 16:19:25 +0100

    added a test to make travis happy

--- tests/public.test.js
+++ tests/public.test.js
@@ -1,1 +1,6 @@
 const test = require('tape');
+
+test('initial setup on public', (t) => {
+  t.equal(1, 1, 'tape is running on public.test.js');
+  t.end();
+});
--- tests/src.test.js
+++ tests/src.test.js
@@ -1,2 +1,7 @@
 const test = require('tape');
 const shot = require('shot');
+
+test('initial setup on src', (t) => {
+  t.equal(1, 1, 'tape is running on src.test.js');
+  t.end();
+});
