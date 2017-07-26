3091925f25017f135f3702e681b3d0c559d2e0ab
ameliejyc <amelie.chan@gmail.com>
2017-07-26 17:23:18 +0100

    added handleHomeRoute and corresponding test :zap: relates #17 #12

--- tests/src.test.js
+++ tests/src.test.js
@@ -1,7 +1,15 @@
 const test = require('tape');
 const shot = require('shot');
+const router = require('../src/router');
 
 test('initial setup on src', (t) => {
   t.equal(1, 1, 'tape is running on src.test.js');
   t.end();
+});
+
+test('checking home route', (t) => {
+  shot.inject(router, {method: 'get', url: '/'}, (res) => {
+    t.equal(res.statusCode, 200, 'should respond with status code 200');
+    t.end();
+  })
 });
