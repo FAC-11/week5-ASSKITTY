d0dea3198b3491cfb41396f9561d73bcf56f7c67
Max Gerber <mgerber94@gmail.com>
2017-07-27 16:07:09 +0100

    removes default value for date parameter in buildAPIURL function

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const env = require('env2')('./config.env');
 
-const buildAPIURL = ( date = 20170727, apiURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+const buildAPIURL = ( date, apiURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
   apiKey = process.env.APIKEY ) => {
   return `${apiURL}?api-key=${apiKey}&begin_date=${date}&end_date=${date}&sort=newest`;
 }
