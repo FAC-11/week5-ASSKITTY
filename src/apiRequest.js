af2a90b1e48847d78d6007a5e59fd9bccd194b22
Max Gerber <mgerber94@gmail.com>
2017-07-27 15:21:41 +0100

    finished makeRequest() function :green_apple: Relates #2 and #4

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,21 +1,15 @@
 /* eslint-disable */
 const request = require('request');
 const env = require('env2')('./config.env');
-const makeRequest = ( apiUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
-apiKey = process.env.APIKEY,
-date ) => {
 
-  request.get({
-    url: apiUrl,
-    qs: {
-      'api-key': process.env.APIKEY,
-      'begin_date': date,
-      'end_date': date,
-      'sort': "newest"
-    },
-  }, function(err, response, body) {
-    body = JSON.parse(body);
-    console.log(body);
+const buildAPIURL = ( date = 20170727, apiURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+  apiKey = process.env.APIKEY ) => {
+  return `${apiURL}?api-key=${apiKey}&begin_date=${date}&end_date=${date}&sort=newest`;
+}
+
+const makeRequest = ( apiUrl = buildAPIURL() ) => {
+  request.get(apiUrl, (err, response, body) => {
+    return JSON.parse(body);
   });
 }
 
